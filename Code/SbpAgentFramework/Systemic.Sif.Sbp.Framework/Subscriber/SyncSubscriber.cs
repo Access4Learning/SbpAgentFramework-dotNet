a7051ceca7e812c76e74e6248856b91c64f004a6
Rafidzal Rafiq <rafidzal.rafiq@systemic.com.au>
2011-11-29 16:02:00 +0800

    Updated the XML documentation in the SyncSubscriber class.

--- Code/SbpAgentFramework/Systemic.Sif.Sbp.Framework/Subscriber/SyncSubscriber.cs
+++ Code/SbpAgentFramework/Systemic.Sif.Sbp.Framework/Subscriber/SyncSubscriber.cs
@@ -22,36 +22,51 @@ namespace Systemic.Sif.Sbp.Framework.Subscriber
 {
 
     /// <summary>
-    /// This class implements the Sync Control Functionality through configuration specified in database tables rather
-    /// than through the default behaviour of the SIF Common Framework.
-    /// It is important to note that if the SIF Common Framework property file turns off sync altogether, then the
-    /// method in this class has no affect (meaning it will be ignored).
+    /// This class manages SIF requests through the use of configuration specified in database tables rather than
+    /// through the default behaviour of the SIF Common Framework. By default, this class checks the database table
+    /// hourly (through the use of RequestFrequency) to determine whether a SIF request is required.
     /// </summary>
-    /// <typeparam name="T">The type of SIF Data Object this Subscriber processes, e.g. StudentPersonal, Schoolnfo, etc.</typeparam>
+    /// <typeparam name="T">The type of SIF Data Object this Subscriber processes, e.g. StudentPersonal, Schoolnfo.</typeparam>
     public abstract class SyncSubscriber<T> : GenericSubscriber<T> where T : SifDataObject, new()
     {
         // Create a logger for use in this class.
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        int requestFrequency = 3600000;
+        int requestFrequency = 3600000; // Hourly.
         ISyncService syncService = new SyncService();
 
+        /// <summary>
+        /// Frequency (in milliseconds) that the database table is checked to determine whether a SIF request is
+        /// required.
+        /// </summary>
         protected override int RequestFrequency
         {
             get { return requestFrequency; }
             set { requestFrequency = value; }
         }
 
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public SyncSubscriber()
             : base()
         {
         }
 
+        /// <summary>
+        /// This constructor specifies the configuration settings associated with this Subscriber.
+        /// </summary>
+        /// <param name="agentConfig">Configuration settings associated with this Subscriber.</param>
         public SyncSubscriber(AgentConfig agentConfig)
             : base(agentConfig)
         {
         }
 
+        /// <summary>
+        /// This method will check the database table to determine whether a SIF request is required.
+        /// </summary>
+        /// <param name="zone">Zone associated with the SIF request.</param>
+        /// <returns>True is a SIF request is required; false otherwise.</returns>
         protected override bool MakeRequest(IZone zone)
         {
             bool makeRequest = syncService.IsSyncRequired(SifObjectType.Name, AgentConfiguration.SourceId, zone.ZoneId);
@@ -60,7 +75,8 @@ namespace Systemic.Sif.Sbp.Framework.Subscriber
         }
 
         /// <summary>
-        /// Override the default behaviour of the SIF Common Framework base Subscriber.
+        /// Override the default behaviour of the SIF Common Framework base Subscriber to mark the associated SIF Data
+        /// Object type as requested once done.
         /// </summary>
         /// <param name="zone">Zone to synchronise with.</param>
         /// <see cref="Systemic.Sif.Framework.Subscriber.GenericSubscriber{T}.BroadcastRequest(Edustructures.SifWorks.IZone)"/>
