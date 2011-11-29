fdb87eff293d592c06c41f9ab0a63b7c7c44dff3
Rafidzal Rafiq <rafidzal.rafiq@systemic.com.au>
2011-11-29 14:36:07 +0800

    Added Apache licensing information to class headers.

--- Code/SbpAgentFramework/Systemic.Sif.Sbp.Framework/Subscriber/Baseline/SchoolInfoSubscriber.cs
+++ Code/SbpAgentFramework/Systemic.Sif.Sbp.Framework/Subscriber/Baseline/SchoolInfoSubscriber.cs
@@ -1,4 +1,19 @@
-﻿using Edustructures.SifWorks.School;
+﻿/*
+* Copyright 2011 Systemic Pty Ltd
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software distributed under the License 
+* is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+* or implied.
+* See the License for the specific language governing permissions and limitations under the License.
+*/
+
+using Edustructures.SifWorks.School;
 using Edustructures.SifWorks.Tools.Cfg;
 
 namespace Systemic.Sif.Sbp.Framework.Subscriber.Baseline
